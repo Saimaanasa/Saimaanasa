abb4595fda0fce1b49f8f24292aade4f63e77948
Saimaanasa <87806207+Saimaanasa@users.noreply.github.com>
2021-08-04 18:45:25 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,53 +1,76 @@
- var Runner;
-  var path,invisibleGround,pathImage
+var garden,rabbit;
+var gardenImg,rabbitImg;
+var leaves,orangeleaves,redleaf;
+
 function preload(){
-  //pre-load images
-
-  pathImg=loadImage("path.png");
-
-  Runner_running = loadAnimation("Runner-1.png","Runner-2.png");
-
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
 }
 
 function setup(){
+  
   createCanvas(400,400);
-  //create sprites here
-  path=createSprite(200,200);
-  path.addImage(pathImg);
-  path.velocityY = 4;
-  path.scale = 1.2;
+  
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
 
-  Runner_running=createSprite(200,200);
-  Runner_running.addAnimation("Runner-1.png","Runner-2.png");
-  Runner_running.velocityY = 0;
-  Runner_running .scale = 0.1;
+//creating boy running
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
 
-invisibleGround=createSprite(10,200,10,400);
-invisibleGround.visible=true;
-
-invisibleGround=createSprite(390,200,10,400);
-invisibleGround.visible=true;
-
+function createApples(){
+  apple=createSprite(Random(50,350),40,10,10);
+  apple.addImage(rabbitImg);
+  apple.scale=0.1;
+  apple.velocityY=-1;
+  apple.lifetime=100;
+  
+}
 }
 
+
 function draw() {
-  background("black");
- 
+  background(0);
+
+  rabbit.x=world.mouseX
+
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  drawSprites();
+}
+var select_sprite=Math.round(Random(1,2));
+if(framecount % 80==0){
+  if(select_sprite==1){
+  }
+    functioncreatapple
+  }
+  else{
+functioncreatleaves
+
+leaves=createSprite(Random(50,350),40,10,10);
+  leaves.addImage(leafImg);
+  leaves.scale=0.1;
+  leaves.velocityY=-1;
+  leaves.lifetime=100;
+
+functionCreateaorangeleaf
+
+orangeleaf=createSprite(Random(50,350),40,10,10);
+orangeleaf.addImage(orangeImg);
+orangeleaf.scale=0.1;
+orangeleaf.velocityY=-1;
+orangelef.lifetime=100;
+
+  functionCreateredleaf
+
+  redleaf=createSprite(Random(50,350),40,10,10);
+  redleaf.addImage(redleafImg);
+  redleaf.scale=0.1;
+  redleaf.velocityY=-1;
+  redleaf.lifetime=100;
+
   
-
-  if(path.y > 400){
-    path.y = height/2;
-    }
-
-    if(keyIsDown(LEFT_ARROW)){
-      Runner=LEFT;
-    }
-
-    if(keyIsDown(RIGHT_ARROW)){
-      Runner=RIGHT;
-
-      
-
-    }
-    drawSprites();
   }
